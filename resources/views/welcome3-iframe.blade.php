d5fe66d564e28e9dba10ec2cc0f21e38d212c3be
wassim.kaouia <58480846+wassim-kaouia@users.noreply.github.com>
2023-06-04 17:12:02 +0100

    update

--- resources/views/welcome3-iframe.blade.php
+++ resources/views/welcome3-iframe.blade.php
@@ -320,8 +320,8 @@
             </div>
         </div>
 
-        <div class="d-flex justify-content-center">
-            <img src="{{ url('assets/images/logo_ovm.png') }}" class="w-25 mb-4 mt-2" alt="">
+        <div class="d-flex justify-content-center mt-4">
+            <img src="{{ url('assets/images/LOGO-MYDIGIHOUSE_new.png') }}" class="w-25 mb-4 mt-2" alt="">
         </div>
 
       </div>
