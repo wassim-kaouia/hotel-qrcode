c92b541672ad03519d50a98c314d3ef5d0478199
wassim.kaouia <58480846+wassim-kaouia@users.noreply.github.com>
2023-09-04 12:19:34 +0100

    ok

--- resources/views/modules/module/index.blade.php
+++ resources/views/modules/module/index.blade.php
@@ -28,7 +28,7 @@
                         <div class="bg-gray-100 shadow-md rounded-md w-40 h-40  mb-8">
                             <div class="p-4">
                                 {{-- <i class="fa fa-calculator text-6xl flex justify-center" aria-hidden="true"></i> --}}
-                                <img src="{{ url('imagesApp/images/icons/'.$settingGeneral->digicode_path) }}" class="icon d-block center-block w-24 mx-auto">
+                                <img src="{{ url('imagesApp/images/icons/'.$settingGeneral->digicode_path) }}" class="icon d-block center-block w-20 mx-auto">
 
                                 <a href="{{ route('digicode.index') }}" class="flex justify-center mb-2 text-2xl mt-2">Digicode</a>
                                
@@ -39,7 +39,7 @@
                         <div class="bg-gray-100 shadow-md rounded-md w-40 h-40  mb-8">
                             <div class="p-4">
                                 {{-- <i class="fa fa-bookmark text-6xl flex justify-center" aria-hidden="true"></i> --}}
-                                <img src="{{ url('imagesApp/images/icons/'.$settingGeneral->livre_path) }}" class="icon d-block center-block w-24 mx-auto">
+                                <img src="{{ url('imagesApp/images/icons/'.$settingGeneral->livre_path) }}" class="icon d-block center-block w-20 mx-auto">
 
                                 <a href="{{ route('livre.index') }}" class="flex justify-center mb-2 text-2xl mt-2">Livre d'or</a>
                                
@@ -50,7 +50,7 @@
                         <div class="bg-gray-100 shadow-md rounded-md w-40 h-40  mb-8">
                             <div class="p-4">
                                 {{-- <i class="fa fa-street-view text-6xl flex justify-center" aria-hidden="true"></i> --}}
-                                <img src="{{ url('imagesApp/images/icons/'.$settingGeneral->arround_path) }}" class="icon d-block center-block w-24 mx-auto">
+                                <img src="{{ url('imagesApp/images/icons/'.$settingGeneral->arround_path) }}" class="icon d-block center-block w-20 mx-auto">
 
                                 <a href="{{ route('alentours.index') }}" class="flex justify-center mb-2 text-2xl mt-2">Alentours</a>
                                 
@@ -61,7 +61,7 @@
                         <div class="bg-gray-100 shadow-md rounded-md w-40 h-40  mb-8">
                             <div class="p-4">
                                 {{-- <i class="fa fa-info text-6xl flex justify-center" aria-hidden="true"></i> --}}
-                                <img src="{{ url('imagesApp/images/icons/'.$settingGeneral->info_path) }}" class="icon d-block center-block w-24 mx-auto">
+                                <img src="{{ url('imagesApp/images/icons/'.$settingGeneral->info_path) }}" class="icon d-block center-block w-20 mx-auto">
 
                                 <a href="{{ route('infos.index') }}" class="flex justify-center mb-2 text-2xl mt-2">Info</a>
                             </div>
@@ -72,7 +72,7 @@
                             <div class="p-4">
                                 {{-- <i class="fa fa-volume-control-phone text-6xl flex justify-center"
                                     aria-hidden="true"></i> --}}
-                                <img src="{{ url('imagesApp/images/icons/'.$settingGeneral->numero_path) }}" class="icon d-block center-block w-24 mx-auto">
+                                <img src="{{ url('imagesApp/images/icons/'.$settingGeneral->numero_path) }}" class="icon d-block center-block w-20 mx-auto">
 
                                 <a href="{{ route('numeros.index') }}" class="flex justify-center mb-2 text-2xl mt-2">Numeros</a>
                                 
@@ -84,7 +84,7 @@
                             <div class="p-4">
                                 {{-- <i class="fa fa-handshake-o text-6xl flex justify-center"
                                     aria-hidden="true"></i> --}}
-                                <img src="{{ url('imagesApp/images/icons/'.$settingGeneral->partenariat_path) }}" class="icon d-block center-block w-24 mx-auto">
+                                <img src="{{ url('imagesApp/images/icons/'.$settingGeneral->partenariat_path) }}" class="icon d-block center-block w-20 mx-auto">
     
                                 <a href="{{ route('partenariat.index') }}" class="flex justify-center mb-2 text-2xl mt-2">Partenariat</a>
                                 
